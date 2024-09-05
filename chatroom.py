37a95fd6d66d2dc092b62099ffa4538afae30c9a
otter <42103041+whatotter@users.noreply.github.com>
2024-09-04 20:22:59 -0500

    frickin crc checking broke

--- chatroom.py
+++ chatroom.py
@@ -2,6 +2,7 @@ import obcb_comms
 import threading
 import json
 import argparse
+import hashlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument("room", help="page* to communicate on")
@@ -11,6 +12,9 @@ args = parser.parse_args()
 sock = obcb_comms.socket(int(args.room))
 previousMsg = None
 die = False
+
+def md5(text):
+    return hashlib.md5(text.encode()).hexdigest()
 
 def rx():
     global previousMsg
@@ -24,11 +28,12 @@ def rx():
                 t = json.loads(a)
 
                 # verify that the fields we need exist
+
                 assert t.get("user", False) != False
                 assert t.get("text", False) != False
                 assert t.get("crc", False) != False
 
-                assert hash(t["text"]) == t["crc"] # verify that the checksum matches the text
+                assert md5(t["text"]) == t["crc"] # verify that the checksum matches the text
             except:
                 continue
             
@@ -45,7 +50,7 @@ def tx():
     while True:
         msg = input("\033[0m\n>") # + reset ansi to prevent annoying ansi codes
 
-        sock.sendall(json.dumps({"user": args.user, "text": msg, "crc": hash(msg)}))
+        sock.sendall(json.dumps({"user": args.user, "text": msg, "crc": md5(msg)}))
 
 a = threading.Thread(target=rx)
 a.start()
